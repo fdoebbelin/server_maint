39f3f0251f95095bab60492377a9603fc44aa109
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2012-10-18 16:40:31 +0200

    fix path error

--- lib/server_maint.rb
+++ lib/server_maint.rb
@@ -2,6 +2,6 @@ require "server_maint/version"
 
 module ServerMaint
   def self.get_cookbook_path
-    File.expand_path('./cookbooks', __FILE__)
+    File.expand_path('../cookbooks', __FILE__)
   end
 end
